16dcca6866b0ff8664b8b80e04c4eb052a9c1a10
alishbah13 <alishbah13@gmail.com>
2021-04-29 23:11:26 +0500

    tfidf

--- vsm.py
+++ vsm.py
@@ -2,15 +2,22 @@
 from nltk.tokenize import RegexpTokenizer, word_tokenize
 from collections import defaultdict
 import json
+import math
 
 
-class postings:
+class vector_space_model:
     def __init__(self):
         self.raw_text = ""
         self.files = [[]]
         self.processed_text = []
         self.index = {}
         self.tokenizer = RegexpTokenizer(r'\w+')
+        self.doc_vectors = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10:[],
+                            11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], 20:[],
+                            21:[], 22:[], 23:[], 24:[], 25:[], 26:[], 27:[], 28:[], 29:[], 30:[],
+                            31:[], 32:[], 33:[], 34:[], 35:[], 36:[], 37:[], 38:[], 39:[], 40:[],
+                            41:[], 42:[], 43:[], 44:[], 45:[], 46:[], 47:[], 48:[], 49:[], 50:[]
+                            }
 
 
     
@@ -59,25 +66,42 @@ class postings:
             self.index[word] = [df, temp] 
         
         return self.index
+    
 
     def store_index(self):
         with open('index.txt', 'w') as file:
             file.write(json.dumps(self.index)) # use `json.loads` to do the reverse
         file.close()
 
-    def tf_idf(query):
+    def tf_idf(self, query):
         #form partial doc vectors
+        # print( self.index)
+        query = list( query.split(" ") )
         for term in query:
             df = self.index[term][0]
-            idf = math.log( df , 10)
+            idf = math.log( 50/df , 10)
 
-            for i in range(df):
+            for i in range(1,51):
+                docs = list( self.index[term][1] )
+                if i in docs:
+                    tfidf = self.index[term][1][i] * idf
+                    self.doc_vectors[i].append(tfidf)
+                else:
+                    self.doc_vectors[i].append(0)
+        
+        return self.doc_vectors
+    
+    def cosine_sim(self):
+        for i in range(1,51):
+            # self.doc_vectors[i]
+
                 
 
 
-x = postings()
+x = vector_space_model()
 x.get_files('ShortStories')
 x.get_raw_text('ShortStories')
 x.preprocess()
-print( x.set_index() )
+x.set_index() 
 x.store_index()
+print(x.tf_idf('write and play'))
